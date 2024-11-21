2566b6a6117e49e2bc08e232cd4fbc815297e6fb
FallenDios <francocabrera585@gmail.com>
2024-11-21 16:55:25 -0300

    Agregue dos funciones nuevas Y tambien una funcion mas de las opciones

--- funcionesMenu.php
+++ funcionesMenu.php
@@ -3,6 +3,17 @@
 /****** FUNCIONES COMPLEMENTARIAS******/
 /**************************************/
 
+
+/**
+ * Funcion diseñada para el menu principal para que cualquier tecla que se ingrese se muestre el menu
+ */
+
+ function presionarEnterContinuar(){
+    escribirAmarillo("Presione ENTER para continuar \n");
+    trim(fgets(STDIN));
+ }
+
+
 /**
  * Funcion que segun la opcion que se le pasa por parametro obtiene una palabra de un arreglo indexado de palabras.
  * En la primera opcion la eleccion de la palabra estara a cargo del usuario.
@@ -31,19 +42,26 @@
  }
 
 
- /**
-  * 
-  */
-
-
-
-
-
-
-
-
-
-
+/** Función que verifica si un jugador ya jugó una partida con una palabra.
+ *  Si ya existe una partida, la función devuelve true, de lo contrario devuelve false.
+* @param ARRAY $partidas
+* @param STRING $nombreJugador 
+* @param STRING $palabra
+* @return BOOLEAN
+*/
+function verificarPalabra($partidas, $nombreJugador, $palabra) {
+    //BOOLEAN $palabraUtilizada
+    //INT $indice
+    $palabraUtilizada = false;
+    $indice = 0;
+    while (!$palabraUtilizada && $indice < count($partidas)) {
+      if (($partidas[$indice]["jugador"]) == $nombreJugador && ($partidas[$indice]["palabraWordix"]) == $palabra) {
+        $palabraUtilizada = true;
+      }
+      $indice ++;
+    }
+    return $palabraUtilizada;
+}
 
 
 
@@ -85,4 +103,23 @@
      }
      return $nuevapartida;
  }
+
+ /**Funcion opcion 3 del menu principal
+  * Pide al usuario un numero de partida y llama a una funcion "MostrarPartida"
+  * pasandole por parametro dicho numero y coleccion de partidas jugadas generadas en la sesion actual
+  * Si la partida existe, muestra sus datos. Caso contrario solicita nuevamente al usuario que ingrese un numero valido.
+  *
+  * @param ARRAY $coleccionPartidas
+  */
+
+  function menuOpcion3($coleccionPartidas){
+    //INT $partidaNro
+
+    echo"Ingrese el numero de partida que desea ver :\n";
+    $partidaNro= solicitarNumeroEntre(1, count($coleccionPartidas));
+    $partidaNro -=1;
+    echo"Buscando partida..... \n";
+    mostrarPartida($partidaNro,$coleccionPartidas);
+  }
+
 
